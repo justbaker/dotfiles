7f8ee0373fc632890cbe8d1164670523f0870de4
Justin A. Baker <justin.baker@tma1.com>
2017-08-14 11:53:31 -0400

    Updated dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 # Helpful aliases
-alias bx="bundle exec"
+alias tl="tail -n1000 -f $@"
+alias fp="ps aux | grep -v grep | grep $@ -i --color=auto"
 alias :q="exit"
-alias :t="tail -n1000 -f $@"
-alias :p="ps aux | grep -v grep | grep $@ -i --color=auto"
+alias ...="cd ../../"
+alias ....="cd ../../../"
